54b5e78db156934eda54aebee3ccb51021502100
Duuun <daimura10@gmail.com>
2019-01-17 00:49:00 +0900

    modify

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
-
+  
   def update
     task = Task.find(params[:id])
     task.update!(task_params)
@@ -25,6 +25,12 @@ class TasksController < ApplicationController
     task = Task.new(task_params)
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
