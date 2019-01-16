fbca037d61ef28c941f8efa70e7d81a80738f9c4
Duuun <daimura10@gmail.com>
2019-01-17 01:10:49 +0900

    Add annotate module, routes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Task < ApplicationRecord
 end
