726897fd7a6534f065df730def69a4e2a2b370fe
Duuun <daimura10@gmail.com>
2019-01-27 15:38:34 +0900

    add before_validation to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,6 +11,8 @@
 #
 
 class Task < ApplicationRecord
+  before_validation :set_name_nameless_name
+
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
@@ -18,5 +20,9 @@ class Task < ApplicationRecord
 
     def validate_name_not_including_comma
       errors.add(:name, 'にカンマを追加することは出来ません.. (泣)') if name&.include?(',')
+    end
+
+    def set_name_nameless_name
+      self.name = '名前はあとで決めるよ！' if name.blank?
     end
 end
